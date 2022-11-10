8b9963c72a780aceef4e9aa5926541408f5e12a6
congibbs10 <congibbs10@gmail.com>
2022-11-09 22:10:17 -0700

    Compare two vectors for equality.

---
+++ R/vector_equal.R
@@ -0,0 +1,17 @@
+#' Check if two vectors are equivalent, avoiding the issue of recycling values.
+#'
+#' @param x A vector.
+#' @param y A vector.
+#'
+#' @return `TRUE` or `FALSE` depending on whether the vectors are equal.
+#' @export
+#'
+#' @examples
+#' x <- c(1, 2, 3)
+#' y <- c(1, 2, 3, 1)
+#' vector_equal(x, y)
+#' all(x == y)
+vector_equal <- function(x, y) {
+  is.numeric(x) && is.numeric(y) && length(x) == length(y) &&
+    all(x == y)
+}
